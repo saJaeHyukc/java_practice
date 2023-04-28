617f1076665d0322d90efebc77c41ef64f65f345
wogur981208 <wogur981208@gmail.com>
2023-04-28 16:45:39 +0900

    Add Number, Datatype class

---
+++ src/Datatype.java
@@ -0,0 +1,17 @@
+public class Datatype {
+    public static void main(String[] args){
+        System.out.println(6); //Number
+        System.out.println("six"); // String
+
+        System.out.println(("6")); // String 6
+
+        System.out.println(6+6); // 12
+        System.out.println("6" + "6"); // String 66
+
+        System.out.println(6 * 6); // 36
+        // System.out.println("6" * "6"); // Error 문자열끼리 곱하기 못함
+
+        System.out.println("1111".length()); // 4
+        // System.out.println(1111.length()); // Error
+    }
+}

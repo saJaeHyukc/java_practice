7ae62e669e22181c1a92bb7c03ed78a4ab330b89
wogur981208 <wogur981208@gmail.com>
2023-04-28 17:14:00 +0900

    Add Letter:

---
+++ src/Letter.java
@@ -0,0 +1,13 @@
+public class Letter {
+    public static void main (String[] args){
+        // 다른 사람이 파악할 수 있도록 코드 작성 ******
+        // * 변수의 효용: 어떤 값을 넣어야 하는 부분에 '변수'를 사용함으로써,
+        // 특정 값의 수정이 필요할 때 변수하나만 수정하면 모두 일괄 수정 적용이 일어나는 폭발적인 효과가 있다.
+        // * 변수의 활용상황: "값이 변할 수 있는 부분", 즉 고객 나이, 직업, 신분 등 정보가 변할 수 있는 부분을 변수로 해 사용하면 유용할 것이다
+        String name = "egoing";
+        System.out.println("Hello,"+name+" ... "+name+" bye" );
+
+        double VAT = 10.0;
+        System.out.println(VAT);
+    }
+}
--- src/Variable.java
+++ src/Variable.java
@@ -2,6 +2,9 @@ public class Variable {
     public static void main(String[] args){
 
         // 자바는 데이터타입을 명확하게 표시해줘야함
+        // 변수의 데이터 타입을 지정하는 이유는 지정해둔 데이터 타입과 다른 값이 입력될 경우
+        // java가 complie하지 않고 걸러주기 때문임
+        // 덕분에 추후 각 변수마다 어떤 데이터 타입의 값이 입력돼있는지 확신
         int a = 1; // Number -> integer
         System.out.println(a); // 1
 

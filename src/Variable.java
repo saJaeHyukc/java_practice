fcd3acd722751dc579c1da11016237b8f238748b
wogur981208 <wogur981208@gmail.com>
2023-04-28 17:09:06 +0900

    Add Variable

---
+++ src/Variable.java
@@ -0,0 +1,15 @@
+public class Variable {
+    public static void main(String[] args){
+
+        // 자바는 데이터타입을 명확하게 표시해줘야함
+        int a = 1; // Number -> integer
+        System.out.println(a); // 1
+
+        double b = 1.1; // real number -> double
+        System.out.println(b); // 1.1
+
+        String c = "Hello World";
+        System.out.println(c); // Hello World
+
+    }
+}

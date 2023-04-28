70c45099412651693027098447dda38b51114332
wogur981208 <wogur981208@gmail.com>
2023-04-28 16:56:05 +0900

    Add String App, README

---
+++ README.md
@@ -0,0 +1,7 @@
+## 생활코딩 JAVA1 [생활코딩 유튜브 링크](https://www.youtube.com/watch?v=-dPXqgWQBGE&list=PLuHgQVnccGMAIluRRVsC1e79ri-dwnBmR&index=1)
+<br>
+
+### 목적
+- 커리어월렛 backend 기여하기 위함
+
+
---
+++ src/StringApp.java
@@ -0,0 +1,16 @@
+public class StringApp {
+    public static void main(String[] args){
+        // Character vs String
+        System.out.println("Hello World"); // String "는 문자열을 표현
+        System.out.println('H'); // Character 자바에서는 '는 한글자만 표현함 * 문자와 문자열은 다름
+        System.out.println("H");
+
+        System.out.println("Hello"
+                + "World");
+       // new line
+        System.out.println("Hello \n World");
+
+        //escape
+        System.out.println("Hello \"World\""); // Hello "World"
+    }
+}

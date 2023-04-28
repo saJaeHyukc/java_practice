9243ba4e6e6baaffb99c2186bdc5f874f95e6287
wogur981208 <wogur981208@gmail.com>
2023-04-28 17:03:08 +0900

    Add StringOperation

---
+++ src/StringOperation.java
@@ -0,0 +1,9 @@
+public class StringOperation {
+    public static void main(String[] args){
+        // 문자열 다루기 http://bit.ly/2UZSzLn
+        System.out.println("Hello World".length()); // 11
+        System.out.println("Hello, leezche ... bye.".replace("leezche", "egoing"));
+        System.out.println("Hello, [[[name]]] ... bye.".replace("[[[name]]]", "egoing"));
+    }
+}
+
